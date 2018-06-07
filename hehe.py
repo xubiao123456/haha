281a596cec0004840d9bab74024e806d7ed9655f
xubiao123456 <37292670+xubiao123456@users.noreply.github.com>
2018-06-07 16:29:44 +0800

    Update hehe.py

--- hehe.py
+++ hehe.py
@@ -1,23 +1,3 @@
 print('美滋滋')
 print('hahahhahaha')
-print(11111111111111111)
-print(2222222222)
-print(3333333)
-print 4444444
 
-print 555
-
-
-print 777
-
-
-print(6666)
-
-
-
-print 8888888
-
-print 999999999
-
-
-print 000000000
