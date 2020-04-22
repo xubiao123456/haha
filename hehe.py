ae07960179176c7189cc2a17259d60cf90435ac6
徐彪 <xubiao@chengfayun.com>
2020-04-22 22:10:20 +0800

    添加测试数据

--- hehe.py
+++ hehe.py
@@ -6,7 +6,12 @@ print 8888888
 print 9999999
 print('今天有点累')
 print 6666
-print('99999999')
-print '66666666'
-print('888888')
-print '777777'
+
+print('cehsi')
+
+print('999888')
+print '999888'
+print('999888')
+print '999888'
+
+
